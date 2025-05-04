c9ac1871a7e6ca50e74c1ea485fc608ffb6f1c69
Valeria Mejia <valemega.vox@gmail.com>
2025-05-03 18:37:41 -0600

    add aletheialogo to navbar

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -1,26 +1,46 @@
 import CustomButton from "./Button";
-import { Grid } from "@mui/material";
+import { Grid, ImageListItem } from "@mui/material";
 import GradientButton from "./gradientButton";
 
 const NavBar = () => {
   return (
     <Grid display="flex" direction="row" spacing={8}>
+      <Grid>
+        <ImageListItem
+          sx={{ width: 188, height: 44, margin: 1, marginRight: 13 }}
+        >
+          <img alt="aletheia logo" src="../public/img/AlétheIABlack.svg" />
+        </ImageListItem>
+      </Grid>
       <Grid display="flex" direction="row">
         <Grid margin={2}>
-          <CustomButton variant="text" label="Inicio"></CustomButton>
+          <CustomButton
+            labelColor="black"
+            variant="text"
+            label="Inicio"
+          ></CustomButton>
         </Grid>
         <Grid margin={2}>
-          <CustomButton variant="text" label="Recursos"></CustomButton>
+          <CustomButton
+            labelColor="black"
+            variant="text"
+            label="Recursos"
+          ></CustomButton>
         </Grid>
         <Grid margin={2}>
           <CustomButton
             variant="text"
             label="Preguntas Frecuentes"
+            labelColor="black"
           ></CustomButton>
         </Grid>
 
         <Grid margin={2}>
-          <CustomButton variant="text" label="Colaboraciones"></CustomButton>
+          <CustomButton
+            labelColor="black"
+            variant="text"
+            label="Colaboraciones"
+          ></CustomButton>
         </Grid>
       </Grid>
 
